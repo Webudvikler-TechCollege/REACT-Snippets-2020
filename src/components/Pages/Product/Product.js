8162ff40221c586823bc076c0dfda4feacb7c3ee
mftw <michaelnlkoch@gmail.com>
2020-06-08 22:29:56 +0200

    small fixes

--- src/components/Pages/Product/Product.js
+++ src/components/Pages/Product/Product.js
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-const getParams = search => {
-  return search.split("?")[1].split("&").reduce((obj, keyvals) => {
-    const [key, val] = keyvals.split("=");
-    obj[key] = val;
-    return obj;
-  }, {})
-}
+const getParams = (url) => {
+  return url
+    .split("?")[1]
+    .split("&")
+    .reduce((obj, keyvals) => {
+      const [key, val] = keyvals.split("=");
+      obj[key] = val;
+      return obj;
+    }, {});
+};
 
 export default function Product(props) {
-  console.log("Product -> slug", getParams(props.location.search));
-  const { id } = getParams(props.location.search)
-  const [data, setData] = useState(null)
-  
+  const { id } = getParams(props.location.search);
+  const [data, setData] = useState(null);
+
   useEffect(() => {
-    if(!data && id) {
+    if (!data && id) {
       fetch("https://api.mediehuset.net/bakeonline/products/" + id)
-      .then(res => res.json()).then(apidata => setData(apidata))
+        .then((res) => res.json())
+        .then((apidata) => setData(apidata));
     }
-  }, [data, setData, id])
+  }, [data, setData, id]);
 
   return (
     <div>
@@ -26,13 +29,10 @@ export default function Product(props) {
         <div>
           <h2>{data.title}</h2>
           <p>{data.teaser}</p>
-
         </div>
       ) : (
-        <div>
-          Loading..
-        </div>
+        <div>Loading..</div>
       )}
     </div>
-  )
+  );
 }
