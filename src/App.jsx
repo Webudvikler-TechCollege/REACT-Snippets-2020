e9f1578e4a1f835b164d4f71210b0d0daebb29e0
Heinz K <heka@techcollege.dk>
2020-08-03 12:22:10 +0200

    Login Leg

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState, Suspense } from 'react';
 import Header from './components/Partials/Header/Header';
 import Main from './components/Partials/Main/Main';
 import Footer from './components/Partials/Footer/Footer';
 import Nav from './components/Partials/Nav/Nav';
 import Router from './components/Router/Router';
+import { SuspenseLoader } from "./components/Contexts/Loader";
+//import ErrorBoundary from "./components/Contexts/ErrorBoundary";
 
 import './App.css';
 
 function App() {
-
   return (
     <>
       <Header>
         <Nav />
       </Header>
       <Main>
-        <Router />
+          <SuspenseLoader>
+            <Router />
+          </SuspenseLoader>
       </Main>
       <Footer></Footer>
     </>
