84ac40fc2cbca070dd2c0a6f8b2924ece39d8b44
Heinz K <heka@techcollege.dk>
2020-08-11 14:24:37 +0200

    Hoteller og destinationer Update

--- src/components/Pages/Overlook/index.jsx
+++ src/components/Pages/Overlook/index.jsx
@@ -4,83 +4,78 @@ import Styles from './overlook.module.scss';
 //import Header from '../../images/iceland.jpg'
 //import Search from '../../Search/Search'
 
-const apiUrl = 'https://api.mediehuset.net/overlook/countries';
+export default function Destinations(props) {
 
-export default function Lineup(props) {
+    // Komponent til divs med lande og hoteller
     const [CountryData, setCountryData] = useState(null);
-        return (
-            <div className="Destinations">
-                <div className="scenes">
+    return (
+        <div className="Destinations">
+            <div className="countrylist">
                 <CountryList setCountryData={setCountryData} />
-                </div>
-                <div className="artists">
-                    <LocationList data={CountryData} />
-                </div>
             </div>
-        )
-    }
+            <div className="citylist">
+                <CityList data={CountryData} />
+            </div>
+        </div>
+    )
+}
 
-    const LocationList = props => {
-        const {data} = props;
-        console.log(props);
-        return (
-            <div className="locationGrid">
-                {/**
-                {data && data.map(item => (
-                    <div key={"location-" + item.id}>
-                        <div className="gridItem">
-                        <Link to={"/location?id=" + item.id}>
-                        <img src={item.image}/>
-                        <h4>{item.name}</h4>
+// Komponent til at loope array med byer
+const CityList = props => {
+    const {data} = props;
+
+    return (
+        <div className="locationGrid">
+            {data && data.map(city => (
+                <div key={"location-" + city.id}>
+                    <div className="gridItem">
+                        <Link to={"/location?id=" + city.id}>
+                            <img alt={city.name} src={city.image} />
+                            <h4>{city.name}</h4>
                         </Link>
-                        </div>
                     </div>
-                ))}
-                */}
-            </div>
-        )
-    }
-    const CountryList = props => {
-        const { setCountryData } = props;
-        const [apiData, setApiData] = useState(null);
-        useEffect(() => {
-            if(!apiData) {
-                fetch(apiUrl)
-                    .then((res) => res.json())
-                    .then((data) => setApiData(data));
-            }
-        }, [apiData, setApiData]);
-        const fetchCountryData = id => {
+                </div>
+            ))}
+        </div>
+    )
+}
+
+// Komponent til at fetche lande liste og detaljer
+const CountryList = props => {
+    const { setCountryData } = props;
+    const [apiData, setApiData] = useState(null);
+
+    // Henter liste af lande
+    useEffect(() => {
+        if(!apiData) {
             fetch('https://api.mediehuset.net/overlook/countries')
                 .then((res) => res.json())
-                .then((data) => {
-                    let destinations = data.items;
-                    let destination = destinations.find(function(item, index) {
-                        if(item.id === id) {
-                            fetch('https://api.mediehuset.net/overlook/countries/' + id)
-                                .then((res2) => res2.json())
-                                .then((location) => {
-                                    let locations = location.item.cities.items;
-                                    return locations;
-                                })
-                        } else {
-                            return null;
-                        }
-                    });
-                    setCountryData(destination)
-                })
-            }
-        return (
-            <div className="HOD">
-                <div className="Grid">
-                    {apiData && apiData?.items.map((item) => {
-                        return <span key={item.id}>
-                                    <Link onClick={e => fetchCountryData(item.id)}>
-                                        {item.name}
-                                    </Link>
-                                </span>
-                    })}    
-        </div>
-            </div>
-        )
+                .then((data) => setApiData(data));
+        }
+    }, [apiData, setApiData]);
+
+    // Henter land detaljer ved klik på et land fra listen
+    const fetchCountryData = id => {
+        fetch('https://api.mediehuset.net/overlook/countries/' + id)
+            .then((res) => res.json())
+            .then((data) => {
+                setCountryData(data.item.cities.items);
+        })
     }
+
+    return (
+        <div className="HOD">
+            <div className="Grid">
+                {apiData && apiData?.items.map((item) => {
+                    return (
+                        <span key={item.id}>
+                            <Link onClick={e => fetchCountryData(item.id)}>
+                                {item.name}
+                            </Link>
+                        </span>
+                    )
+                })}    
+            </div>
+        </div>
+    )
+}
